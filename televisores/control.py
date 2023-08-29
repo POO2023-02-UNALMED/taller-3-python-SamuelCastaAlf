71fe7df563c941cabc7b8c2a690c9f402fbce379
SamuelCastaAlf <92555846+SamuelCastaAlf@users.noreply.github.com>
2023-08-28 22:34:30 -0500

    Correc en control
    Prueba

--- televisores/control.py
+++ televisores/control.py
@@ -3,28 +3,20 @@ class Control:
         self._tv=tv
         tv.setControl(self)
     def turnOn(self):
-        if(self._tv is not None):
             self._tv.turnOn()
     def turnOff(self):
-        if(self._tv is not None):
             self._tv.turnOff()
     def canalUp(self):
-        if(self._tv is not None):
             self._tv.canalUp()
     def canalDown(self):
-        if(self._tv is not None):
             self._tv.canalDown()
     def volumenUp(self):
-        if(self._tv is not None):
             self._tv.volumenUp()
     def volumenDown(self):
-        if(self._tv is not None):
             self._tv.volumenDown()
     def setCanal(self,canal):
-        if(self._tv is not None):
             self._tv.setCanal(canal)
     def setVolumen(self,volumen):
-        if(self._tv is not None):
             self._tv.setVolumen(volumen)
     def get_tv(self):
         return self._tv
